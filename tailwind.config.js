96626ad148cb4d84b5ca7f15e94f3c2aaeebc04a
Ragum Z Y <ragumzy@Ragums-MacBook-Pro.local>
2025-07-04 12:48:59 +0700

    add phosphor icon and change tailwind CDN into CLI using postcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./views/**/*.twig",
+    "./*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
